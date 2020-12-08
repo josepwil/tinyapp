abd50b92f133586dd901d91f99c5bf2b658433be
Joseph Williams <josephwilliamsjs@gmail.com>
2020-12-08 01:04:46 +0000

    implemented getRandomString function

--- express_server.js
+++ express_server.js
@@ -6,6 +6,11 @@ const PORT = 8080; // default port 8080
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
+// function to generate random string
+const generateRandomString = () => {
+  return Math.random().toString(36).slice(2, 8);
+}
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -19,10 +24,16 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase};
   res.render('urls_index.ejs', templateVars);
 });
-// form to add a new url
+// get form to add a new url
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
+// post url data from form
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 // specific url
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
